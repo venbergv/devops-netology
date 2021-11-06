6593414c03e22e7885be391f482f8afe43c407c3
Vitaly Grebnev <vitaly.grebnev@gmail.com>
2021-11-06 03:45:34 +0300

    Merge branch 'git-merge'

--- 02-git-03-branching/rebase.sh
+++ 02-git-03-branching/rebase.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # display command line options
 
+
 count=1
 for param in "$@"; do
-    echo "\$@ Parameter #$count = $param"
+    echo "Next parameter: $param"
     count=$(( $count + 1 ))
 done
 
